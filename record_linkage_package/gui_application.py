6018732b2b0065a32d6e4ed532598d91fb323571
Terresa Tran <tran10@uw.edu>
2025-11-28 16:43:34 -0800

    allow users to zoom in on an image

--- record_linkage_package/gui_application.py
+++ record_linkage_package/gui_application.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_image_zoom import image_zoom
+from PIL import Image
 
 st.title("Record Linkage GUI")
+st.sidebar.header("Controls")
+
+if "zoom" not in st.session_state: 
+    st.session_state.zoom = 1.0
+
+
 
 def load_csv():
     uploaded_csv = st.file_uploader('Upload a CSV', type="csv", accept_multiple_files=False)
@@ -18,10 +26,14 @@ else:
     st.warning("Please upload a csv file.")
 
 
-def upload_image_folder():
-    image_folder = st.file_uploader("Upload folder of images", accept_multiple_files=True, type=["jpg","jpeg","png"])
-    if image_folder:
-        for image_file in image_folder:
-            st.image(image_file)
 
-upload_image_folder()
+def upload_image_files():
+    image_files = st.file_uploader("Upload images", accept_multiple_files=True, type=["jpg","jpeg","png"])
+    if image_files:
+        for image_file in image_files:
+            image = Image.open(image_file)
+            image_width, image_height = image.size
+
+            image_zoom(image, mode="scroll", size=(int(image_width/2), int(image_height/2)), keep_aspect_ratio=False, zoom_factor=4.0, increment=0.2)
+
+upload_image_files()
