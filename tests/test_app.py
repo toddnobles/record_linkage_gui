b5eb681d56f807e7eb4c5a732ab65cbbe007118f
Natalie Turner <nturner2@uw.edu>
2025-11-30 16:25:26 -0800

    natalie adding 4 tests

---
+++ tests/test_app.py
@@ -0,0 +1,120 @@
+"""
+Tests for app.py 
+"""
+
+import pytest           
+import pandas as pd 
+from app import load_data, find_image, get_image_map
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Natalie Tests: 
+
+def test_smoke_find_image():
+    """
+    author: nturner2
+    reviewer: honglamv7/juliaz35
+    category: smoke test
+    """
+    # does find_image work
+    # requires inputs of filename and image_map (dictionary)
+    filename = "fake.png"
+    image_map = {"fake.png": "fake_object"}
+
+    find_image(filename, image_map)
+    return 
+
+def test_edge_load_data_excel(tmp_path):
+    """
+    author: nturner2
+    reviewer: honglamv7/juliaz35
+    category: edge test
+    """
+    # needed a temporary file; had to get help with that 
+    # something that is not in distribution - excel
+    excel_file = tmp_path / "file.xlsx"
+    excel_file.write_text("this is not a CSV")
+
+    # test that load_data fails
+    with pytest.raises(pd.errors.ParserError):
+        load_data(excel_file)
+
+def test_oneshot_load_data(tmp_path):
+    """
+    author: nturner2
+    reviewer: honglamv7/juliaz35
+    category: one shot test
+    """
+    # test something where we know the outcome
+    # can create what's expected for load_data (a csv)
+    csv_file = tmp_path / "example.csv"
+    csv_file.write_text("col1,col2\n1,a\n2,b")
+    df = load_data(csv_file)
+
+    assert df.at[0, "col1"] == 1
+    assert df.at[1, "col2"] == "b"
+    
+def test_pattern_find_image():
+    """
+    author: nturner2
+    reviewer: honglamv7/juliaz35
+    category: pattern test
+    """
+    # test something where we know the outcome for multiple inputs
+    # can create what's expected for find_image 
+    # can do something like one shot test but with more?
+    image_map = {"ex1.png": "object_ex1", 
+                 "ex2.png": "object_ex2",
+                 "ex3.png": "object_ex3"}
+    assert find_image("ex1.png", image_map) == "object_ex1"
+    assert find_image("ex2.png", image_map) == "object_ex2"
+    assert find_image("ex3.png", image_map) == "object_ex3"
+
+    # Not in here should get None
+    assert find_image("ex4.png", image_map) is None
+
