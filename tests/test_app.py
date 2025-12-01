d682d187f39b0f9e7c4e58695944e1105066b60e
Honglam <honglamv7@gmail.com>
2025-11-30 20:22:55 -0800

    create testing outline

---
+++ tests/test_app.py
@@ -0,0 +1,25 @@
+import pytest
+from record_linkage.app import load_data, get_image_map, find_image, render_viewer, main
+
+def smoke_test_hv():
+    """
+    author: honglamv7
+    reviewers: nturner27, juliaz35
+    """
+
+def one_shot_hv():
+    """
+    author: honglamv7
+    reviewers: nturner27, juliaz35
+    """
+
+def edge_test_hv():
+    """
+    author: honglamv7
+    reviewers: nturner27, juliaz35
+    """
+def pattern_test_hv():
+    """
+    author: honglamv7
+    reviewers: nturner27, juliaz35
+    """
