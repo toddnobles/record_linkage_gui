42441fff29e57c35d8b209077e7dcd29a0187818
Todd Nobles <tonobles@gmail.com>
2025-12-02 20:43:07 -0500

    adding fourth test and note for why it's not a pattern test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,31 @@
 """Tests for app.py"""
 import pytest
-import pandas as pd
-from record_linkage.app import load_data, main
+from streamlit.testing.v1 import AppTest 
+from record_linkage.app import load_data
 
-def test_smoke():
+def test_application_runs():
     """
     author: toddnobles
     reviewer: tttran01
     category: smoke test
     """
-    main()
+    at = AppTest.from_file("src/record_linkage/app.py").run()
+    assert not at.exception
     return
 
-## edge test
+
 def test_not_csv_upload():
     """
     author: toddnobles
     reviewer: tttran01
     category: edge test
+    Testing that the load_data function behaves as expected when we give it a non-CSV filepath. 
     """
     with pytest.raises(Exception):
         # we ideally want the function to raise a better error here. Currenlty this is caught in the main() function, but we probably want to exract it to be self-contained here. (I think)
         load_data("test.pdf")
     return
 
-## oneshot test
 def test_load_data():
     """
     author: toddnobles
@@ -33,21 +34,20 @@ def test_load_data():
     Testing that an uploaded CSV actually loads.
     """
     df = load_data("tests/test_data/sample_data.csv")
-    # we probably want a better function here that tells the user that the CSV is empty if this fails
+    # we probably want a better function in the main app that tells the user that the CSV is empty if this fails
     assert not df.empty
     return
 
-## pattern test
-def pattern_test_tn ():
+def test_initial_ui_render():
     """
     author: toddnobles
     reviewer: tttran01
-    category: pattern test (still working on this one)
+    category: smoke test
+    Adding another (sort of) smoke test for the app logic of the order steps and sections are displayed to the user. 
+    I can't figure out how to get the user interactivity testing in Streamlit to work for file uploads. There is currently no file_upload testing in Streamlit's AppTest. Additionally, I don't think we have pattern test as an option here as we do no calculations. So patterns could only be tested if we can simulate user inputs which we can't currently do for file uploads (which our app requires to run properly). Using this to get experience with the AppTest framework.  
     """
-    input_df = pd.read_csv("tests/test_data/sample_data.csv")
-    main()  # Run the main app function to simulate user running the app
-    output_df = df ## here df is the output of main(). We can't really simulate user inputs here.
-    assert list(input_df.columns) == list(output_df.columns)
-    return
-
-
+    at = AppTest.from_file("src/record_linkage/app.py").run()
+    assert at.header[0].value == "1. Upload Data"
+    ## confirming that the second section hasn't rendered yet since no CSV has been uploaded
+    header_values = [h.value for h in at.header]
+    assert "2. Configure Mapping" not in header_values
